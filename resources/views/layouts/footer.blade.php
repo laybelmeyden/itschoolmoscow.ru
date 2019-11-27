c7ad20f24dfd2f1268f756911946d70b8d280cb0
laybelmeyden <trycollens@gmail.com>
2019-11-27 16:53:24 +0300

    da

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,43 +1,9 @@
 <footer id="footer">
 <div class="flex_footer">
 @if(session('message'))
-<div style="
-    background: #9FCE0D;
-    box-shadow: 6px 0px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 0px 213.5px 213.5px 0px;
-    min-width: 98%;
-    height: 498px;
-    position: absolute;
-    margin-right: 57px;
-    margin-top: -41px;
-    z-index: 9999;
-    text-align: center;">
-    <h2 style="
-    text-align:center;
-    font-style: normal;
-    font-weight: 300;
-    font-size: 36px;
-    line-height: 14px;
-    /* or 39% */
-    
-    color: #000000;
-    margin: 0;
-    padding-top: 155px;
-    padding-left: 185px;
-    padding-right: 48px;
-    ">{{ session('message') }}</h2>
-    <p style="
-    font-style: normal;
-    font-weight: 300;
-    font-size: 22px;
-    line-height: 30px;
-    /* or 136% */
-    
-    color: #000000;
-    margin: 0;
-    padding-top: 30px;
-    padding-left: 185px;
-    padding-right: 48px;">{{ session('message_1') }}</p>
+<div class="footer_modal" style="">
+    <h2>{{ session('message') }}</h2>
+    <p>{{ session('message_1') }}</p>
     </div>
     @endif 
     <div class="block_left">
