2b9b1e11ba91e604f12ada33bcbda34398700b8d
laybelmeyden <trycollens@gmail.com>
2019-12-23 17:30:35 +0300

    da

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -49,7 +49,9 @@
         <div class="col col_f">
             <p>Сайт разработан с использованием средств Гранта Мэра Москвы для социально ориентированных некоммерческих организаций</p>
         </div>
+        {literal}
 <!-- Yandex.Metrika informer --> <a href="https://metrika.yandex.ru/stat/?id=56694640&amp;from=informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/56694640/1_0_3F3359FF_3F3359FF_1_pageviews" style="width:80px; height:15px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры)" class="ym-advanced-informer" data-cid="56694640" data-lang="ru" /></a> <!-- /Yandex.Metrika informer --> <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(56694640, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/56694640" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 <!-- /Yandex.Metrika counter -->
+{/literal}
 </div>
 </footer>
