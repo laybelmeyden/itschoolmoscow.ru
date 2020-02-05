161161a1bb40e9c7362f2d97bdf9c0f71e7c7cbd
laybelmeyden <trycollens@gmail.com>
2020-02-05 22:28:35 +0300

    ada

--- resources/views/pages/patch12.blade.php
+++ resources/views/pages/patch12.blade.php
@@ -6,12 +6,10 @@
 <div class="container">
 <div class="row">
 <div class="green_header_uchast">
-    <h1>Троицкий АДМИНИСТРАТИВНЫЙ ОКРУГ</h1>
-    <h3>дата мероприятия будет известна чуть позже</h3>
-    <p>г. Москва, Большая Новодмитровская ул., 36, стр. 12</p>
-<div class="col">
-<a class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
-</div>
+    <h1>ТРОИЦКИЙ АДМИНИСТРАТИВНЫЙ ОКРУГ</h1>
+    <h3>10-11 марта 2020 год</h3>
+    <p style="line-height: 20px;">"Гимназия г. Троицка"</p>
+    <p style="line-height: 20px;">г. Москва, г. Троицк, Октябрьский проспект, д.6, аудиторрии №33,38 (3-й этаж)</p>
 </div>
 </div>
 </div>
@@ -20,24 +18,69 @@
 <div class="container">
     <div class="row">
         <h2>ПРОГРАММА МЕРОПРИЯТИЯ</h2>
-        <p><a>Здесь скоро появится программа</a></p>
+        <div class="grid_two">
+          <div>
+          <p><a>Дата: 10 марта, Время: 13:30 до 15:00</a></p>
+          <p><a>День 1 (6-8 класс)</a></p>
+          <div class="district_btn">
+          <div class="district_btn_flex">
+          <a href="https://vk.com/doc95748576_534614000?hash=072b9491ef421a7b2f&dl=fb64ac0f474debbb99" class="waves-effect waves-light btn btn_footer">ПРОГРАММА</a>
+          <a href="https://rusinnovations.timepad.ru/event/1252622/" class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
+          </div>
+          </div>
+          </div>
+          <div>
+          <p><a>Дата: 11 марта, Время: 13:30 до 15:00</a></p>
+          <p><a>День 2 (9-11 класс)</a></p>
+          <div class="district_btn">
+          <div class="district_btn_flex">
+          <a href="https://vk.com/doc95748576_534614048?hash=41894cbd9d9b6b7ad8&dl=a11bfc0784da7a9cb3" class="waves-effect waves-light btn btn_footer">ПРОГРАММА</a>
+          <a href="https://rusinnovations.timepad.ru/event/1252922/" class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
+          </div>
+          </div>
+          </div>
+        </div>
     </div>
 </div>
 </section>
-<section id="rucovodstvo" style="margin-top:73px; margin-bottom:194px;">
+<section id="rucovodstvo" style="margin-top:73px;">
 <div class="container">
   <div class="row">
     <div class="flex_ruko" style="background-color:#F7F7F7;background-image: url(assets/img/Group223.png);">
       <div class="img_ruko">
-        <img src="assets/img/sp4.png" alt="">
+        <img src="assets/img/13532801_1141651809241466_1941085025128508361_n.jpg" alt="" style="width: 100%;">
       </div>
       <div class="text_ruko">
         <p style="color: #5D4A86;">КУРАТОР ШКОЛЫ</p>
-        <h2 style="color: #3F3359;">Владимир Шульгин</h2>
-        <h3 style="color: #3F3359;">Руководитель проектов АНО "АИР"</h3>  
+        <h2 style="color: #3F3359;">Токмакова Ирина Вячеславовна</h2>
+        <h3 style="color: #3F3359;">заместитель директора. <br>Контакты: 89037539460, shabanova73@mail.ru</h3>  
       </div>
     </div>
   </div>
 </div>
+</section>
+<section id="speakers" style="margin-bottom:60px;">
+  <div class="container">
+    <div class="row">
+    <h1 style="margin-top: 100px;">СПИКЕРЫ</h1>
+    <div class="flex_all_speak">
+    <div class="flex_speak">
+    <div class="img_speak">
+        <img src="assets/img/bp1L6XBkzDM.jpg" alt="">
+      </div>
+      <div class="text_speak">
+        <p>Андрей Черный</p>
+        <h2>МГТУ им. Баумана, судья Московской олимпиады по робототехнике, преподаватель, автор креативных методик обучения детей и взрослых, победитель всероссийской научно-технической конференции студентов «Студенческая научная весна 2019: Машиностроительные технологии»</h2>
+      </div>
+    </div>
+    </div>
+    </div>
+  </div>
+</section>
+<section id="maps_district">
+  <div class="maps_district_analog">
+    <p>"Гимназия г. Троицка", г. Москва, г. Троицк, Октябрьский проспект, д.6, аудиторрии №33,38 (3-й этаж)</p>
+  </div>
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A07a64a5f4fcc475a38592d4e2da3c924b755174fb452eb61039b21e6264ae27f&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
 </section>
 @endsection
