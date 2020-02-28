5cee969765b36f3a5ff2bf81dfa474fe9ebf83e0
laybelmeyden <trycollens@gmail.com>
2020-02-28 18:04:13 +0300

    dadda

--- resources/views/pages/patch8.blade.php
+++ resources/views/pages/patch8.blade.php
@@ -6,12 +6,10 @@
 <div class="container">
 <div class="row">
 <div class="green_header_uchast">
-    <h1>Юго-Западный АДМИНИСТРАТИВНЫЙ ОКРУГ</h1>
-    <h3>дата мероприятия будет известна чуть позже</h3>
-    <p>г. Москва, Большая Новодмитровская ул., 36, стр. 12</p>
-<div class="col">
-<a class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
-</div>
+    <h1>ЮГО-ЗАПАДНЫЙ АДМИНИСТРАТИВНЫЙ ОКРУГ</h1>
+    <h3>16-17 марта 2020 год</h3>
+    <p style="line-height: 20px;">Государственное бюджетное общеобразовательное учреждение города Москвы"Школа №2009" ( ГБОУ Школа №2009)</p>
+    <p style="line-height: 20px;">Москва., ул. Бартеневская, д.49, корп.4 Актовый зал, 2 этаж. ауд 200</p>
 </div>
 </div>
 </div>
@@ -20,24 +18,69 @@
 <div class="container">
     <div class="row">
         <h2>ПРОГРАММА МЕРОПРИЯТИЯ</h2>
-        <p><a>Здесь скоро появится программа</a></p>
+        <div class="grid_two">
+          <div>
+          <p><a>Дата: 16 марта, Время: 12:30 до 14:10</a></p>
+          <p><a>День 1 (6-8 класс)</a></p>
+          <div class="district_btn">
+          <div class="district_btn_flex">
+          <a href="https://vk.com/doc32870057_537479849?hash=aa81be6520e6f893f3&dl=8f1f2d57b63c72229d" class="waves-effect waves-light btn btn_footer">ПРОГРАММА</a>
+          <a href="https://rusinnovations.timepad.ru/event/1254275/" class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
+          </div>
+          </div>
+          </div>
+          <div>
+          <p><a>Дата: 17 марта, Время: 11:35 до 13:15</a></p>
+          <p><a>День 2 (9-11 класс)</a></p>
+          <div class="district_btn">
+          <div class="district_btn_flex">
+          <a href="https://vk.com/doc32870057_537479851?hash=7204db13c8dc443602&dl=96ed611f2bcbf34d82" class="waves-effect waves-light btn btn_footer">ПРОГРАММА</a>
+          <a href="https://rusinnovations.timepad.ru/event/1254283/" class="waves-effect waves-light btn btn_footer">УЧАСТВОВАТЬ</a>
+          </div>
+          </div>
+          </div>
+        </div>
     </div>
 </div>
 </section>
-<section id="rucovodstvo" style="margin-top:73px; margin-bottom:194px;">
+<section id="rucovodstvo" style="margin-top:73px;">
 <div class="container">
   <div class="row">
     <div class="flex_ruko" style="background-color:#F7F7F7;background-image: url(assets/img/Group223.png);">
       <div class="img_ruko">
-        <img src="assets/img/sp4.png" alt="">
+        <img src="assets/img/Logotip-2.jpg" alt="" style="width: 100%;">
       </div>
       <div class="text_ruko">
         <p style="color: #5D4A86;">КУРАТОР ШКОЛЫ</p>
-        <h2 style="color: #3F3359;">Владимир Шульгин</h2>
-        <h3 style="color: #3F3359;">Руководитель проектов АНО "АИР"</h3>  
+        <h2 style="color: #3F3359;">Ремизова Елена Георгиевна</h2>
+        <h3 style="color: #3F3359;">Контакты: remizova.eg@sch2009.net, +79166827935</h3>  
       </div>
     </div>
   </div>
 </div>
+</section>
+<section id="speakers" style="margin-bottom:60px;">
+  <div class="container">
+    <div class="row">
+    <h1 style="margin-top: 100px;">СПИКЕРЫ</h1>
+    <div class="flex_all_speak">
+    <div class="flex_speak">
+    <div class="img_speak">
+        <img src="assets/img/bp1L6XBkzDM.jpg" alt="">
+      </div>
+      <div class="text_speak">
+        <p>Андрей Черный</p>
+        <h2>МГТУ им. Баумана, судья Московской олимпиады по робототехнике, преподаватель, автор креативных методик обучения детей и взрослых, победитель всероссийской научно-технической конференции студентов «Студенческая научная весна 2019: Машиностроительные технологии»</h2>
+      </div>
+    </div>
+    </div>
+    </div>
+  </div>
+</section>
+<section id="maps_district">
+  <div class="maps_district_analog">
+    <p>Государственное бюджетное общеобразовательное учреждение города Москвы"Школа№2009" ( ГБОУ Школа №2009)</p>
+  </div>
+  <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A426bdd7a995c78fae364676e2f75a97a73e1ce813713806de957ab3a145d827e&amp;source=constructor" width="100%" height="400" frameborder="0"></iframe>
 </section>
 @endsection
